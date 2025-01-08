fb4cf3e5201d5716dce2e5066de25c3d0ddf891e
goldmaster631026 <goldmaster631026@gmail.com>
2025-01-08 16:41:03 +1000

    164001082025

--- Projects/hello_cargo/src/main.rs
+++ Projects/hello_cargo/src/main.rs
@@ -5,8 +5,21 @@ struct User {
     sign_in_count : u64,
 }
 
+struct Color (i32, i32, i32);
+
+
+fn build_user(email : String, username : String) -> User {
+    User {
+        active : true,
+        username,
+        email,
+        sign_in_count : 1,
+    }
+}
+
 fn main() {
 
+    let black = Color (0,0,10);
     let mut user1 = User {
         email : String::from ("someone@example.com"),
         username : String::from ("someusername123"),
@@ -16,5 +29,9 @@ fn main() {
     user1.email = String::from ("yukiamano221@gmail.com");
     println!("Hello, world!");
     println!("{}",user1.email);
+
+    let mut user2 : User = build_user ( "asd_mail".to_string(), "my_name".to_string());
+    println!("{}",user2.email);
+    println!("{}", black.2);
 
 }
