a9b6dc63fc7689376eef8e2ec07695435b4342fe
goldmaster631026 <goldmaster631026@gmail.com>
2025-01-08 18:39:42 +1000

    Struct

--- Projects/hello_cargo/src/main.rs
+++ Projects/hello_cargo/src/main.rs
@@ -5,8 +5,22 @@ struct User {
     sign_in_count : u64,
 }
 
+#[derive(Debug)]
+struct Reactangle {
+    width : u32,
+    height : u32,
+}
+
+impl Reactangle {
+    fn can_hold(&self, other: &Reactangle) ->bool {
+        self.width> other.width && self.height > other.height
+    }
+}
 struct Color (i32, i32, i32);
 
+fn area (dimensions : (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
 
 fn build_user(email : String, username : String) -> User {
     User {
@@ -16,9 +30,25 @@ fn build_user(email : String, username : String) -> User {
         sign_in_count : 1,
     }
 }
+fn area_struct ( react : & Reactangle) ->u32 {
+    react.width * react.height
+}
 
 fn main() {
+    let rect1 = (30,50);
 
+    let rect2 = Reactangle {
+        width : 30,
+        height : 60,
+    };
+    let rect3 = Reactangle {
+        width: 30,
+        height: 50,
+    };
+    let rect4 = Reactangle {
+        width: 10,
+        height: 60,
+    };
     let black = Color (0,0,10);
     let mut user1 = User {
         email : String::from ("someone@example.com"),
@@ -34,5 +64,10 @@ fn main() {
     println!("{}",user2.email);
     println!("{}", black.2);
     println!("{}", black.0);
-
+    println! ("{}", area(rect1));
+    println! ("{}", area_struct(&rect2));
+    // println!("{}", rect1.height);
+    println!("{}", rect2.height);
+    println! ("{rect2:?}");
+    println!("Can rect3 hold rect4? {}", rect3.can_hold(&rect4));
 }
